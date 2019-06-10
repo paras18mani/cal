c5156d329f139a8fa401d0f1a575e6aa8c98a1ea
rishabhanandshrivastava <rishabhanandshrivastava@gmail.com>
2019-06-10 09:53:37 +0530

    cal

---
+++ calu.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class cal
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Enter the number");
+		Scanner scan = new Scanner(System.in);
+		int a=scan.nextInt();
+		int b=scan.nextInt();
+		
+		System.out.println("Sum of a and b is" + (a+b));
+	}
+}
